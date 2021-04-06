8e8afa467059fb8dbbd419aa622bb3d5afcab9fd
temurbek-4224 <78374407+temurbek-4224@users.noreply.github.com>
2021-04-06 20:52:00 +0500

    we have added header component to our app and link to homePage shop page contact page. Thank u Yihua and Neagoie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 
 
 const HatsPage = () => (
@@ -14,6 +15,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
